6b39d92c2c47524ec404a87d9e3273c319b6c2f0
puzant <PBAKJEJIAN@hotmail.com>
2018-04-11 16:53:20 +0300

    adding new contents and effects

--- japan/effects.js
+++ japan/effects.js
@@ -1,15 +1,33 @@
-$(".anchorLink").click(function(e){
-  e.preventDefault();
- 
-  var this_offset = $(this).offset();
-  var that_id     = $(this).attr("href");
-  var that_offset = $(that_id).offset();
-  var offset_diff = Math.abs(that_offset.top - this_offset.top);
- 
-  var base_speed  = 100; // Time in ms per 1,000 pixels
-  var speed       = (offset_diff * base_speed) / 400;
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {
+      $("#return-to-top").fadeIn(200); 
+    } else {
+      $("#return-to-top").fadeOut(200); 
+    }
+  });
+  $("#return-to-top").click(function() {
+    // When arrow is clicked
+    $("body,html").animate(
+      {
+        scrollTop: 0
+      },
+      500
+    );
+  });
   
-  $("html,body").animate({
-    scrollTop: that_offset.top
-  }, speed);
-});
+  
+  $(".anchorLink").click(function(e){
+    e.preventDefault();
+   
+    var this_offset = $(this).offset();
+    var that_id     = $(this).attr("href");
+    var that_offset = $(that_id).offset();
+    var offset_diff = Math.abs(that_offset.top - this_offset.top);
+   
+    var base_speed  = 100; // Time in ms per 1,000 pixels
+    var speed       = (offset_diff * base_speed) / 400;
+    
+    $("html,body").animate({
+      scrollTop: that_offset.top
+    }, speed);
+  });
